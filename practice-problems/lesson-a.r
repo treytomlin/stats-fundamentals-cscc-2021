5f4d7712f246cca10bf6c80bb20abb3bed7e2992
Trey Tomlin <treytomlin91@gmail.com>
2021-01-14 19:31:09 -0500

    end of class notes, will update this weekend after finished with exercises

--- practice-problems/lesson-a.r
+++ practice-problems/lesson-a.r
@@ -1,3 +1,16 @@
 #practice problem 2
 #2a
 pnorm(q=1.77, mean=0, sd=1)
+#ANSWER -> 0.962
+
+#2b
+1-pnorm(q=0.41)
+#ANSWER -> 0.341
+
+#2c
+pnorm(q=1.37)-pnorm(q=-2.12)
+#ANSWER -> 0.898
+
+#2b
+qnorm(p=0.21)
+#ANSWER -> Z = -0.806
