8abac2a7433f2c513008543eb38e72285889dbf0
Trey Tomlin <treytomlin91@gmail.com>
2021-01-28 20:11:12 -0500

    adds lesson a practice problems

--- practice-problems/lesson-a.r
+++ practice-problems/lesson-a.r
@@ -1,16 +1,55 @@
 #practice problem 2
+
 #2a
 pnorm(q=1.77, mean=0, sd=1)
-#ANSWER -> 0.962
+#ANSWER => 0.962
 
 #2b
 1-pnorm(q=0.41)
-#ANSWER -> 0.341
+#ANSWER => 0.341
 
 #2c
 pnorm(q=1.37)-pnorm(q=-2.12)
-#ANSWER -> 0.898
+#ANSWER => 0.898
 
 #2b
 qnorm(p=0.21)
-#ANSWER -> Z = -0.806
+#ANSWER => Z = -0.806
+
+#practice problem 3
+
+#3a
+pnorm(q=1.26, mean=0, sd=1)
+#Answer => =0.8962
+
+#3b
+pnorm(q=250, mean=266, sd=16)
+#ANSWER => 0.1587
+
+#3c
+1 - pnorm(q=35, mean=28, sd=3)
+#ANSWER => 0.0098
+
+#3d
+pnorm(q=2, mean=0, sd=1) - pnorm(q=-1, mean=0, sd=1)
+#ANSWER => 0.8186
+
+#3e
+1 - pnorm(q=1.5, mean=0, sd=1) + pnorm(q=-2, mean=0, sd=1)
+#ANSWER => 0.0896
+
+#4a
+qnorm(p=0.95, mean=0, sd=1)
+#ANSWER => 1.645
+
+#4b
+qnorm(0.5)
+#ANSWER => 0
+
+#4c
+qnorm(p=0.84, mean=45, sd=6)
+#ANSWER => 50.97
+
+#4d
+qnorm(p=0.77, mean=-7, sd=9)
+#ANSWER => -0.3504
