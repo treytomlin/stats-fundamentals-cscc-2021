998da739f233b4da9766e9f653d196e1ae4d8a15
Trey Tomlin <treytomlin91@gmail.com>
2021-02-06 09:58:48 -0500

    adds lesson a & b post test answers

--- practice-problems/lesson-a-post-test.r
+++ practice-problems/lesson-a-post-test.r
@@ -1,1 +1,41 @@
 #Lesson A Post-Test
+
+#Question 1
+1 - pnorm(q=37000, mean=32000, sd=2500)
+#ANSWER => 0.023
+
+#Question 2
+pnorm(q=35000, mean=32000, sd=2500)  - pnorm(q = 30000, mean=32000, sd=2500)
+#ANSWER => 0.673
+
+#Question 3
+pnorm(q=2.13, mean=0, sd=1) - pnorm(q=-1.25, mean=0, sd=1)
+#ANSWER => 0.878
+                              
+#Question 4
+pnorm(q=1.45, mean=0, sd=1)
+#ANSWER => 0.926
+
+#Question 5
+qnorm(p = 0.40, mean = 0, sd = 1)
+#ANSWER => -0.253
+
+#Question 6
+qnorm(p=0.36)
+#ANSWER => -0.358
+
+#Question 7
+qnorm(p=0.80, mean=0, sd=1)
+qnorm(p=0.20, mean=0, sd=1)
+#ANSWER => 0.842
+
+#Question 8
+(72 - 65) / 2.5
+#ANSWER => 2.8 CORRECT
+
+#Question 9
+1 - pnorm(q=72, mean=65, sd=2.5)
+#ANSWER => 0
+
+#Question 10
+#ANSWER => 68.75
---
+++ practice-problems/lesson-b-post-test.r
@@ -0,0 +1,20 @@
+#Question 1
+1 - pnorm(q=26.5, mean=27, sd=0.387)
+
+#Question 2
+1 - pnorm(q=27.5, mean=27, sd=0.387)
+
+#Question 3
+qnorm(p=0.85, mean=27, sd=0.387)
+
+#Question 4
+pnorm(q=27.2, mean=27, sd=0.387)
+
+#Question 5
+pnorm(q=26.8, mean=27, sd=0.387) - pnorm(q=26, mean=27, sd=0.387)
+
+#Question 8
+pnorm(q=.80, mean=.75, sd=.016)
+
+#Question 9
+1 - pnorm(q=.65, mean=.75, sd=.016)
